34858cb7ac94bf9192b345f81e2ade59adcb5cb9
JiyaItDeveloper <160260198+JiyaItDeveloper@users.noreply.github.com>
2025-02-12 15:28:50 +0500

    .

---
+++ Milestone1/formScript.ts
@@ -0,0 +1,92 @@
+
+let myname = document.getElementById("name")
+console.log('myname:',myname);
+
+
+let  degn= document.getElementById("degn")
+console.log('degn:',degn);
+
+
+let  phone= document.getElementById("phone")
+console.log('phone:',phone);
+
+
+let email= document.getElementById("email")
+console.log('email:',email);
+
+
+let add= document.getElementById("add")
+console.log('add:',add);
+
+
+let pass= document.getElementById("pass")
+console.log('pass:',pass);
+
+let deg= document.getElementById("deg")
+console.log('deg:',deg);
+
+
+let uni= document.getElementById("uni")
+console.log('uni:',uni);
+
+let pass1= document.getElementById("pass1")
+console.log('pass1:',pass1);
+
+let deg1= document.getElementById("deg1")
+console.log('deg1:',deg1);
+
+
+let uni1= document.getElementById("uni1")
+console.log('uni1:',uni1);
+
+
+let skill1= document.getElementById("skill1")
+console.log('skill1:',skill1);
+
+
+let skill2= document.getElementById("skill2")
+console.log('skill2:',skill2);
+
+
+let skill3= document.getElementById("skill3")
+console.log('skill3:',skill3);
+
+
+let lang= document.getElementById("lang")
+console.log('lang:',lang);
+
+
+let lang2= document.getElementById("lang2")
+console.log('lang2:',lang);
+
+
+let styear= document.getElementById("styear")
+console.log('styear:',styear);
+
+
+let endyear= document.getElementById("endyear")
+console.log('endyear:',endyear);
+
+
+let comp= document.getElementById("comp")
+console.log('comp:',comp);
+
+
+let location1= document.getElementById("location")
+console.log('location:',location);
+
+
+let job= document.getElementById("job")
+console.log('job:',job);
+
+
+let achive1= document.getElementById("achive1")
+console.log('achive1:',achive1);
+
+
+let achive2= document.getElementById("achive2")
+console.log('achive2:',achive2);
+
+
+let achive3= document.getElementById("achive3")
+console.log('achive3:',achive3);
